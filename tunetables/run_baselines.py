36a83fe2fdf4b30818115403fc36f2c2351b13e1
Ben Feuer <bf996@nyu.edu>
2024-03-20 10:47:23 +0000

    baselines

--- tunetables/run_baselines.py
+++ tunetables/run_baselines.py
@@ -36,7 +36,7 @@ def eval_method(splits, device, method, cat_idx, metric_used, max_time=300):
     eval_sets_x = torch.cat([splits[1][0], splits[2][0]])
     eval_sets_y = torch.cat([splits[1][1], splits[2][1]])
 
-    if method in ['random_forest', 'lightgbm', 'autogluon', 'autosklearn2', 'cocktail', 'knn']:
+    if method in ['random_forest', 'lightgbm', 'autogluon', 'autosklearn2', 'cocktail', 'knn', 'logistic', 'gp']:
         _, outputs, best_configs = clf(
             splits[0][0],
             splits[0][1],
@@ -58,29 +58,85 @@ def eval_method(splits, device, method, cat_idx, metric_used, max_time=300):
     
     return outputs, best_configs
 
-def run_eval(dataset_name, base_path, max_time, epsilon):
+def run_eval(dataset_name, base_path, max_time, epsilon, local_args):
+
+    def run_and_get_metrics():
+        results = dict()
+        start_time = time.time()
+        outputs, best_configs = eval_method(splits, device, method, [], metric_used, max_time=args.max_time)
+        end_time = time.time()
+        run_time = end_time - start_time
+        results[f'Run_Time'] = np.round(run_time, 3).item()
+        outputs = outputs[:, 0:num_classes]
+        predictions = np.argmax(outputs, axis=1)
+
+        # Divide outputs and predictions into val and test splits
+        val_outputs = outputs[:len(y_val)]
+        val_predictions = predictions[:len(y_val)]
+        test_outputs = outputs[len(y_val):]
+        test_predictions = predictions[len(y_val):]
+
+        results[f'Val_Accuracy'] = np.round(accuracy_score(y_val, val_predictions), 3).item()
+        results[f'Val_Log_Loss'] = np.round(log_loss(y_val, val_outputs, labels=np.arange(num_classes)), 3).item()
+        results[f'Val_F1_Weighted'] = np.round(f1_score(y_val, val_predictions, average='weighted'), 3).item()
+        results[f'Val_F1_Macro'] = np.round(f1_score(y_val, val_predictions, average='macro'), 3).item()
+        try:
+            if num_classes == 2:
+                results['Val_ROC_AUC'] = np.round(roc_auc_score(y_val, val_outputs[:, 1], labels=np.arange(num_classes)), 3).item()
+            else:
+                results['Val_ROC_AUC'] = np.round(roc_auc_score(y_val, val_outputs, labels=np.arange(num_classes), multi_class='ovr'), 3).item()
+        except Exception as e:
+            print("Error calculating ROC AUC: ", e)
+            results['Val_ROC_AUC'] = 0.0
+        results['Val_ECE'] = np.round(um.ece(y_val, val_outputs, num_bins=30), 3).item()
+        results['Val_TACE'] = np.round(um.tace(y_val, val_outputs, num_bins=30), 3).item()
+        results[f'Test_Accuracy'] = np.round(accuracy_score(y_test, test_predictions), 3).item()
+        results[f'Test_Log_Loss'] = np.round(log_loss(y_test, test_outputs, labels=np.arange(num_classes)), 3).item()
+        results[f'Test_F1_Weighted'] = np.round(f1_score(y_test, test_predictions, average='weighted'), 3).item()
+        results[f'Test_F1_Macro'] = np.round(f1_score(y_test, test_predictions, average='macro'), 3).item()
+        try:
+            if num_classes == 2:
+                results['Test_ROC_AUC'] = np.round(roc_auc_score(y_test, test_outputs[:, 1], labels=np.arange(num_classes)), 3).item()
+            else:
+                results['Test_ROC_AUC'] = np.round(roc_auc_score(y_test, test_outputs, labels=np.arange(num_classes), multi_class='ovr'), 3).item()
+        except Exception as e:
+            print("Error calculating ROC AUC: ", e)
+            results['Test_ROC_AUC'] = 0.0
+        results['Test_ECE'] = np.round(um.ece(y_test, test_outputs, num_bins=30), 3).item()
+        results['Test_TACE'] = np.round(um.tace(y_test, test_outputs, num_bins=30), 3).item()
+        if isinstance(best_configs, pd.DataFrame) or isinstance(best_configs, pd.Series):
+            save_path = os.path.join(base_path, model_string + ".csv")
+            best_configs.to_csv(save_path)
+            wandb.save(save_path)
+        elif isinstance(best_configs, dict) and best_configs.get('best') is not None:
+            best_configs = dict(best_configs, **{f"Best_Config_{k}" : v for k, v in best_configs['best'].items()})
+            #drop key 'best'
+            best_configs.pop('best')
+            results = dict(results, **best_configs)
+        return results
+
     print("Running evaluation for dataset: ", dataset_name)
 
     metrics = [tabular_metrics.auc_metric, tabular_metrics.cross_entropy]
 
     # This is the metric used for fitting the models
     metric_used = tabular_metrics.auc_metric
-    # methods = ['random_forest', 'lightgbm', 'cocktail', 'logistic', 'gp', 'knn', 'catboost', 'xgb', 'autosklearn2', 'autogluon']
-    methods = ['autogluon']
+    methods = ['random_forest', 'logistic', 'catboost', 'xgb', 'autosklearn2']
+    # 'lightgbm', 'cocktail', 'autogluon', 'gp', 'knn'
+    # methods = ['autogluon']
     # methods = ['knn']
     device = '0'
 
     config = dict()
     config['dataset'] = dataset_name
     config['base_path'] = base_path
-    config['max_time'] = max_time
     config['metric_used'] = str(metric_used)
     config['device'] = device
     config['methods'] = ", ".join(str(x) for x in methods)
     config['subset_features'] = -1
     config['subset_rows'] = -1
 
-    args = argparse.Namespace(**config)
+    args = argparse.Namespace(**config, **vars(local_args))
 
     # Get dataset
     dataset_path = os.path.join(base_path, dataset_name)
@@ -101,6 +157,9 @@ def run_eval(dataset_name, base_path, max_time, epsilon):
                                         epsilon_vals=epsilon_vals)
 
     for i, split_dictionary in enumerate(dataset.split_indeces):
+
+        if i not in args.splits:
+            continue
         # TODO: make stopping index a hyperparameter
         train_index = split_dictionary["train"]
         val_index = split_dictionary["val"]
@@ -142,74 +201,30 @@ def run_eval(dataset_name, base_path, max_time, epsilon):
             config['split'] = i
             config['n_configs'] = 100
             model_string = f"{dataset_name}" + '_' + f"{method}" + '_split_' + f"{i}" + '_' + datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+            # if args.privacy_sweep:
+            #     model_string += f'_eps_{epsilon}'
             wandb.login(key=get_wandb_api_key())
             wandb.init(config=config, name=model_string, group='baselines',
                 project='tt-dp-strongbaselines', entity='nyu-dice-lab')
             num_classes = len(np.unique(y_train))
-            results = dict()
-            # try:
-            start_time = time.time()
-            outputs, best_configs = eval_method(splits, device, method, [], metric_used, max_time=config['max_time'])
-            end_time = time.time()
-            run_time = end_time - start_time
-            results[f'Run_Time'] = np.round(run_time, 3).item()
-            outputs = outputs[:, 0:num_classes]
-            predictions = np.argmax(outputs, axis=1)
-
-            # Divide outputs and predictions into val and test splits
-            val_outputs = outputs[:len(y_val)]
-            val_predictions = predictions[:len(y_val)]
-            test_outputs = outputs[len(y_val):]
-            test_predictions = predictions[len(y_val):]
-
-            results[f'Val_Accuracy'] = np.round(accuracy_score(y_val, val_predictions), 3).item()
-            results[f'Val_Log_Loss'] = np.round(log_loss(y_val, val_outputs, labels=np.arange(num_classes)), 3).item()
-            results[f'Val_F1_Weighted'] = np.round(f1_score(y_val, val_predictions, average='weighted'), 3).item()
-            results[f'Val_F1_Macro'] = np.round(f1_score(y_val, val_predictions, average='macro'), 3).item()
             try:
-                if num_classes == 2:
-                    results['Val_ROC_AUC'] = np.round(roc_auc_score(y_val, val_outputs[:, 1], labels=np.arange(num_classes)), 3).item()
-                else:
-                    results['Val_ROC_AUC'] = np.round(roc_auc_score(y_val, val_outputs, labels=np.arange(num_classes), multi_class='ovr'), 3).item()
+                results = run_and_get_metrics()
             except Exception as e:
-                print("Error calculating ROC AUC: ", e)
-                results['Val_ROC_AUC'] = 0.0
-            results['Val_ECE'] = np.round(um.ece(y_val, val_outputs, num_bins=30), 3).item()
-            results['Val_TACE'] = np.round(um.tace(y_val, val_outputs, num_bins=30), 3).item()
-            results[f'Test_Accuracy'] = np.round(accuracy_score(y_test, test_predictions), 3).item()
-            results[f'Test_Log_Loss'] = np.round(log_loss(y_test, test_outputs, labels=np.arange(num_classes)), 3).item()
-            results[f'Test_F1_Weighted'] = np.round(f1_score(y_test, test_predictions, average='weighted'), 3).item()
-            results[f'Test_F1_Macro'] = np.round(f1_score(y_test, test_predictions, average='macro'), 3).item()
-            try:
-                if num_classes == 2:
-                    results['Test_ROC_AUC'] = np.round(roc_auc_score(y_test, test_outputs[:, 1], labels=np.arange(num_classes)), 3).item()
-                else:
-                    results['Test_ROC_AUC'] = np.round(roc_auc_score(y_test, test_outputs, labels=np.arange(num_classes), multi_class='ovr'), 3).item()
-            except Exception as e:
-                print("Error calculating ROC AUC: ", e)
-                results['Test_ROC_AUC'] = 0.0
-            results['Test_ECE'] = np.round(um.ece(y_test, test_outputs, num_bins=30), 3).item()
-            results['Test_TACE'] = np.round(um.tace(y_test, test_outputs, num_bins=30), 3).item()
-            if isinstance(best_configs, pd.DataFrame) or isinstance(best_configs, pd.Series):
-                save_path = os.path.join(base_path, model_string + ".csv")
-                best_configs.to_csv(save_path)
-                wandb.save(save_path)
-            elif isinstance(best_configs, dict) and best_configs.get('best') is not None:
-                best_configs = dict(best_configs, **{f"Best_Config_{k}" : v for k, v in best_configs['best'].items()})
-                #drop key 'best'
-                best_configs.pop('best')
-                results = dict(results, **best_configs)
+                print("Error running evaluation: ", e)
+                results = dict()
             wandb.log(results)
             wandb.finish()
 
 if __name__ == '__main__':
     # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset_path', type=str, default='./tunetables/data')
+    parser.add_argument('--dataset_path', type=str, default='./data')
     parser.add_argument('--datasets', type=str, default='./tunetables/metadata/subset.txt', help='Path to datasets text file')
     parser.add_argument('--max_time', type=int, default=300, help='Allowed run time (in seconds)')
     parser.add_argument('--privacy_sweep', action='store_true', help='Run privacy sweep')
     parser.add_argument('--rand_seed', type=int, default=42, help='Random seed')
+    parser.add_argument('--private_val', action='store_true', help='Use private validation set')
+    parser.add_argument('--splits', nargs='+', type=int, default=[0], help='Splits to run')
 
     args = parser.parse_args()
 
@@ -220,6 +235,6 @@ if __name__ == '__main__':
         epsilon = None
         if args.privacy_sweep:
             for tgt_eps in ["0.01", "0.1", "0.5", "1.0", "5.0"]:
-                run_eval(dataset, args.dataset_path, args.max_time, tgt_eps)
+                run_eval(dataset, args.dataset_path, args.max_time, tgt_eps, args)
         else:
-            run_eval(dataset, args.dataset_path, args.max_time, epsilon)
+            run_eval(dataset, args.dataset_path, args.max_time, epsilon, args)
